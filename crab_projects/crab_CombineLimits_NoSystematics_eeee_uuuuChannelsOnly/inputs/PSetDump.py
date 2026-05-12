19289e8c08b11cb6a94ad7cd7bc23e24becf4b88
Blake Hennings <bhennings23@icloud.com>
2026-05-12 10:50:46 -0500

    CRAB

--- crab_projects/crab_CombineLimits_NoSystematics_eeee_uuuuChannelsOnly/inputs/PSetDump.py
+++ crab_projects/crab_CombineLimits_NoSystematics_eeee_uuuuChannelsOnly/inputs/PSetDump.py
@@ -2,9 +2,8 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("NoSplit")
 
-process.source = cms.Source("EmptySource",
-    firstRun = cms.untracked.uint32(1),
-    numberEventsInRun = cms.untracked.uint32(1)
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('')
 )
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1),
